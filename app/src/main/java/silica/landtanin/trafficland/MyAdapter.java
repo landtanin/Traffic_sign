90b453ee8baac827b19445132ab436233770a4f8
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-08-23 16:10:54 +0700

    explicit 3 fields in MyAdapter

--- app/src/main/java/silica/landtanin/trafficland/MyAdapter.java
+++ app/src/main/java/silica/landtanin/trafficland/MyAdapter.java
@@ -1,7 +1,15 @@
 package silica.landtanin.trafficland;
 
+import android.content.Context;
+
 /**
  * Created by landtanin on 8/23/15 AD.
  */
 public class MyAdapter {
-}
+
+    //Explicit
+    private Context objContext;
+    private int[] trafficInts;
+    private String[] trafficStrings;
+
+} //Main class
