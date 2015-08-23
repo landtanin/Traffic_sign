8e534038b0567c4edee3ec774195b05433dea525
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-08-23 15:20:55 +0700

    Create empty clickAboutMe method

--- app/src/main/java/silica/landtanin/trafficland/MainActivity.java
+++ app/src/main/java/silica/landtanin/trafficland/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    } //Main Method
+
+    public void clickAboutMe(View view) {
+
+    } //click
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
