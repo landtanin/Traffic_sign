30fe8b76760ef2f9ca0bcc9725e789e1ab9058c7
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-08-24 10:14:32 +0700

    create title method in MyData class and add 20 strings

--- app/src/main/java/silica/landtanin/trafficland/MyData.java
+++ app/src/main/java/silica/landtanin/trafficland/MyData.java
@@ -4,4 +4,36 @@ package silica.landtanin.trafficland;
  * Created by landtanin on 8/24/15 AD.
  */
 public class MyData {
-}
+
+    public String[] title() {
+
+        String[] strTitle = new String[20];
+        strTitle[0] = "ห้ามเลียวซ้าย";
+        strTitle[1] = "ห้ามเลียวขวา";
+        strTitle[2] = "ให้เดินรถทางเดียวไปข้างหน้า";
+        strTitle[3] = "เลียวขวา";
+        strTitle[4] = "เลียวซ้าย";
+        strTitle[5] = "ทางออก";
+        strTitle[6] = "ทางเข้า";
+        strTitle[7] = "ทางออก";
+        strTitle[8] = "หยุดรถ";
+        strTitle[9] = "จำกัดความสุง 2.5 เมตร";
+        strTitle[10] = "ให้เลี้ยวซ้ายหรือเลี้ยวขวา";
+        strTitle[11] = "ห้ามกลับรถไปทางขวา";
+        strTitle[12] = "ห้ามจอดรถ";
+        strTitle[13] = "ให้รถสวนทางมาก่อน";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "ทางเข้า";
+        strTitle[16] = "โปรดหยุดรถ";
+        strTitle[17] = "จำกัดความเร็ว 50 km/hr";
+        strTitle[18] = "ห้ามรถกว้างเกินกำหนด";
+        strTitle[19] = "ห้ามรถสูงเกินกำหนด";
+
+        return new String[0];
+    }
+
+
+} // Main Class
+
+
+
